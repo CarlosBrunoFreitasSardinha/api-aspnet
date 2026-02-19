bada80487979f91926172dee943f0de71fdfe1ea
CarlosBrunoFreitasSardinha <carlosifto@gmail.com>
2026-02-19 07:49:17 -0300

    chore: Estrutura de camadas internas

---
+++ CB.BackDefault.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CB.BackDefault.Domain
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ CB.BackDefault.Test.Validations/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace CB.BackDefault.Test.Validations
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
