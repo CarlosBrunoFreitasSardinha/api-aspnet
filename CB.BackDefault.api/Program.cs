75a5f089dccaade682a5d0769d925f3ed1c8ebdb
CarlosBrunoFreitasSardinha <carlosifto@gmail.com>
2026-02-19 12:55:40 -0300

    chore: configuração porta padrão e swagger

--- CB.BackDefault.api/Program.cs
+++ CB.BackDefault.api/Program.cs
@@ -4,14 +4,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
+
+
+// Adiciona e configura o Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+
+    // Habilita o Swagger e a UI interativa
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
