100117eada989852eb7d55c4dfff76c2ff8bc4f5
carlos.bruno <carlosifto@gmail.com>
2026-02-20 11:57:15 -0300

    chore: Implementação UnitOfWork pattern and interface

---
+++ CB.BackDefault.Domain/Shared/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace CB.BackDefault.Domain.Shared.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        Task<int> CommitAsync();
+    }
+}
---
+++ CB.BackDefault.Infra.Data/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using CB.BackDefault.Domain.Shared.Interfaces;
+using CB.BackDefault.Infra.Data.Context;
+
+namespace CB.BackDefault.Infra.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private DbFactory _dbFactory;
+
+        public UnitOfWork(DbFactory dbFactory)
+        {
+            _dbFactory = dbFactory;
+        }
+
+        public Task<int> CommitAsync()
+        {
+            return _dbFactory.DbContext.SaveChangesAsync();
+        }
+    }
+}
