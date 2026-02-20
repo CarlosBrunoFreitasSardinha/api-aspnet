3d9a17ea0095c11ba7b548aedb5b6a182ce2b83a
carlos.bruno <carlosifto@gmail.com>
2026-02-20 11:59:14 -0300

    chore(domain, infra): adicionar interface e implementação genérica de repositório

---
+++ CB.BackDefault.Domain/Shared/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+namespace CB.BackDefault.Domain.Shared.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task AdicionarAsync(T entidade);
+        Task AdicionarRangeAsync(List<T> entidade);
+        void Atualizar(T entidade);
+        Task<IEnumerable<T>> ObterTodosAsync(Expression<Func<T, bool>> expressao);
+        Task<T> ObterAsync(Expression<Func<T, bool>> expressao);
+    }
+}
---
+++ CB.BackDefault.Infra.Data/Repository.cs
@@ -0,0 +1,56 @@
+﻿using CB.BackDefault.Domain.Shared.Interfaces;
+using CB.BackDefault.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CB.BackDefault.Infra.Data
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbFactory _dbFactory;
+        private DbSet<T> _dbSet;
+
+        protected DbSet<T> Dbset
+        {
+            get => _dbSet ??= _dbFactory.DbContext.Set<T>();
+        }
+
+        public Repository(DbFactory dbFactory)
+        {
+            _dbFactory = dbFactory;
+        }
+
+        public async Task AdicionarAsync(T entidade)
+        {
+            await Dbset.AddAsync(entidade);
+        }
+
+        public async Task AdicionarRangeAsync(List<T> entidade)
+        {
+            await Dbset.AddRangeAsync(entidade);
+        }
+
+        public void Atualizar(T entidade)
+        {
+            Dbset.Update(entidade);
+        }
+
+        public async Task<IEnumerable<T>> ObterTodosAsync(Expression<Func<T, bool>> expressao)
+        {
+            if (expressao != null)
+            {
+                return await Dbset.Where(expressao).ToListAsync();
+            }
+            return new List<T>();
+        }
+
+        public async Task<T> ObterAsync(Expression<Func<T, bool>> expressao)
+        {
+            if (expressao != null)
+            {
+                return await Dbset.FirstAsync(expressao);
+            }
+            return null;
+        }
+    }
+}
