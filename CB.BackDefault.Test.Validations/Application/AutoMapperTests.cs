5ca2b015262c8b4e972f52816ff4bbc3192e426a
carlos.bruno <carlosifto@gmail.com>
2026-02-21 16:40:21 -0300

    test: Adicionar testes unitários para perfis do AutoMapper

---
+++ CB.BackDefault.Test.Validations/Application/AutoMapperTests.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CB.BackDefault.Application.AutoMapper;
+
+namespace CB.BackDefault.UnitTests.Base
+{
+    public class AutoMapperTests
+    {
+        [Fact]
+        public void Deve_Ter_ConfiguracaoValida_Do_AutoMapper()
+        {
+
+            var config = new MapperConfiguration(cfg => {
+                cfg.AddProfile<AutoMapperProfile>();
+            });
+
+            config.AssertConfigurationIsValid();
+        }
+    }
+}
