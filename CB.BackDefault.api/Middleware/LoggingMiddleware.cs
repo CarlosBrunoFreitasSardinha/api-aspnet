3cb4ad69e7574f59a57c7642bb8c819f97365377
carlos.bruno <carlosifto@gmail.com>
2026-03-05 17:43:47 -0300

    chore(api): middleware logs correlacionados

---
+++ CB.BackDefault.api/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace CB.BackDefault.Api.Middlewares;
+
+public class LoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<LoggingMiddleware> _logger;
+
+    public LoggingMiddleware(RequestDelegate next,
+                             ILogger<LoggingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var correlationId = context.Items[CorrelationIdMiddleware.ItemKey]?.ToString();
+        var stopwatch = Stopwatch.StartNew();
+
+        _logger.LogInformation(
+            "Request: {Method} {Path}",
+            correlationId,
+            context.Request.Method,
+            context.Request.Path);
+
+        await _next(context);
+
+        stopwatch.Stop();
+
+        _logger.LogInformation(
+            "Response: {StatusCode} em {Elapsed}ms",
+            correlationId,
+            context.Response.StatusCode,
+            stopwatch.ElapsedMilliseconds);
+    }
+}
