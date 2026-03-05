4192adcddda883d64f77e641b1b64f8c64077c8a
carlos.bruno <carlosifto@gmail.com>
2026-03-05 17:43:28 -0300

    chore(api): middleware rastreabilidade

---
+++ CB.BackDefault.api/Middleware/CorrelationIdMiddleware.cs
@@ -0,0 +1,27 @@
+﻿namespace CB.BackDefault.Api.Middlewares;
+
+public class CorrelationIdMiddleware
+{
+    private const string HeaderName = "X-Correlation-Id";
+    public const string ItemKey = "CorrelationId";
+    private readonly RequestDelegate _next;
+
+    public CorrelationIdMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (!context.Request.Headers.TryGetValue(HeaderName, out var correlationId))
+        {
+            correlationId = Guid.NewGuid().ToString();
+            context.Request.Headers[HeaderName] = correlationId;
+        }
+
+        context.Items[ItemKey] = correlationId.ToString();
+        context.Response.Headers[HeaderName] = correlationId;
+
+        await _next(context);
+    }
+}
