5c9a8fc2e025a2a08ef0c89b4cd15064e0bcbc7a
carlos.bruno <carlosifto@gmail.com>
2026-03-15 15:11:08 -0300

    feat(Infra): Mappeamento da Classe RefreshToken

---
+++ CB.BackDefault.Infra.Data/Aggregates/AuthAggregate/Mappings/RefreshTokenMap.cs
@@ -0,0 +1,17 @@
+﻿using CB.BackDefault.Domain.Aggregates.AuthAggregate.Models;
+using CB.BackDefault.Infra.Data.Shared.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CB.BackDefault.Infra.Data.Aggregates.AuthAggregate.Mappings
+{
+    public static class RefreshTokenMap
+    {
+        public static void AddRefreshToken(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RefreshTokenModel>()
+                        .HasOne<UserAplication>()
+                        .WithMany(u => u.RefreshTokens)
+                        .HasForeignKey(r => r.UserId);
+        }
+    }
+}
