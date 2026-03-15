fe01b51f431d352ad575a5663b32949bba6bab35
carlos.bruno <carlosifto@gmail.com>
2026-03-15 15:03:07 -0300

    feat(Application): Criação do Serviço de Token

---
+++ CB.BackDefault.Application/Aggregates/AuthAggregate/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CB.BackDefault.Application.Aggregates.AuthAggregate.Interfaces
+{
+    public interface ITokenService
+    {
+        Task<string> GenerateAccessToken(IdentityUser user);
+    }
+}
---
+++ CB.BackDefault.Application/Aggregates/AuthAggregate/Services/TokenService.cs
@@ -0,0 +1,47 @@
+﻿using CB.BackDefault.Application.Aggregates.AuthAggregate.Interfaces;
+using CB.BackDefault.Application.Shared.Settings;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CB.BackDefault.Application.Aggregates.AuthAggregate.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly JwtSettings _jwtSettings;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public TokenService(JwtSettings jwtSettings, UserManager<IdentityUser> userManager)
+        {
+            _jwtSettings = jwtSettings;
+            _userManager = userManager;
+        }
+
+        public async Task<string> GenerateAccessToken(IdentityUser user)
+        {
+            var claims = await _userManager.GetClaimsAsync(user);
+
+            var tokenClaims = new List<Claim>(claims)
+                                    {
+                                        new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                                        new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                                    };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Secret));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                                issuer: _jwtSettings.Issuer,
+                                audience: _jwtSettings.Audience,
+                                claims: tokenClaims,
+                                expires: DateTime.UtcNow.AddMinutes(15),
+                                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
