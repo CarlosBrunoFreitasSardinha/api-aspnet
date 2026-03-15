84ff34bcb6f6865c56d7db8e71b7e5cb5c8b7975
carlos.bruno <carlosifto@gmail.com>
2026-03-15 15:05:35 -0300

    feat(Domain): Criação da Classe RefreshToken

---
+++ CB.BackDefault.Domain/Aggregates/AuthAggregate/Models/RefreshTokenModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CB.BackDefault.Domain.Aggregates.AuthAggregate.Models
+{
+    [Table("RefreshToken")]
+    public class RefreshTokenModel
+    {
+        public Guid Id { get; set; }
+
+        public string TokenHash { get; set; } = null!;
+
+        public string UserId { get; set; } = null!;
+
+        public DateTime ExpirationDate { get; set; }
+
+        public bool Revoked { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; }
+
+        public Guid? ReplacedByTokenId { get; set; }
+
+        public DateTime? RevokedAt { get; set; }
+
+        public string? CreatedByIp { get; set; }
+
+        public string? RevokedByIp { get; set; }
+
+        public bool IsExpired => DateTime.UtcNow >= ExpirationDate;
+        public bool IsActive => !Revoked && !IsExpired;
+
+        public RefreshTokenModel() { }
+        public RefreshTokenModel(Guid id, string tokenHash, string userId, DateTime createdAt, DateTime expirationDate, string createByIp)
+        {
+            Id = id;
+            TokenHash = tokenHash;
+            UserId = userId;
+            CreatedAt = createdAt;
+            ExpirationDate = expirationDate;
+            CreatedByIp = createByIp;
+        }
+    }
+}
