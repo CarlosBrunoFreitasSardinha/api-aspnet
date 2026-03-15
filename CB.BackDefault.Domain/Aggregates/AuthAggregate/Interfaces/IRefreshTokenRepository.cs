5bae9f58b26d813d9dd3cd9fbafe24cb49e73e94
carlos.bruno <carlosifto@gmail.com>
2026-03-15 15:07:24 -0300

    feat(Domain, Infra):Criação da Interface e do Repository de RefreshToken

---
+++ CB.BackDefault.Domain/Aggregates/AuthAggregate/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,9 @@
+﻿using CB.BackDefault.Domain.Aggregates.AuthAggregate.Models;
+using CB.BackDefault.Domain.Shared.Interfaces;
+
+namespace CB.BackDefault.Domain.Aggregates.AuthAggregate.Interfaces
+{
+    public interface IRefreshTokenRepository : IRepository<RefreshTokenModel>
+    {
+    }
+}
---
+++ CB.BackDefault.Infra.Data/Aggregates/AuthAggregate/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,18 @@
+﻿using CB.BackDefault.Domain.Aggregates.AuthAggregate.Interfaces;
+using CB.BackDefault.Domain.Aggregates.AuthAggregate.Models;
+using CB.BackDefault.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CB.BackDefault.Infra.Data.Aggregates.AuthAggregate.Repositories
+{
+    public class RefreshTokenRepository : Repository<RefreshTokenModel>, IRefreshTokenRepository
+    {
+        private readonly DbSet<RefreshTokenModel> _dbSet;
+
+        public RefreshTokenRepository(DbFactory dbFactory) : base(dbFactory)
+        {
+            _dbSet = dbFactory.DbContext.Set<RefreshTokenModel>();
+        }
+    }
+}
