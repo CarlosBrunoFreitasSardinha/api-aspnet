a170f7bb2ec6a2ef2ce5aa0f494d63583a6e5997
carlos.bruno <carlosifto@gmail.com>
2026-03-15 16:12:04 -0300

    refactor(TestIntegration): Base integrada pra endpoints que exigem autenticacao

---
+++ CB.BackDefault.IntegrationTests/Base/BaseAuthenticatedTest.cs
@@ -0,0 +1,31 @@
+﻿using CB.BackDefault.IntegrationTests.Collections;
+using System.Net.Http.Headers;
+
+namespace CB.BackDefault.IntegrationTests.Base;
+
+[Collection(nameof(IntegrationApiCollection))]
+public abstract class BaseAuthenticatedTest<TProgram> where TProgram : class
+{
+    protected readonly IntegrationTestsFixture<TProgram> Fixture;
+
+    protected BaseAuthenticatedTest(IntegrationTestsFixture<TProgram> fixture)
+    {
+        Fixture = fixture;
+    }
+
+    protected async Task<HttpClient> CreateAuthenticatedClientAsync()
+    {
+        var (email, senha) = Fixture.GerarUsuarioCredenciais();
+
+        await Fixture.CriarUsuarioAsync(email, senha);
+
+        var token = await Fixture.ObterTokenAsync(email, senha);
+
+        var client = Fixture.Factory.CreateClient();
+
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", token);
+
+        return client;
+    }
+}
