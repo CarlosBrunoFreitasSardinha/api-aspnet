b6df06688ea80a0c33faffaa05486da1de687fc1
carlos.bruno <carlosifto@gmail.com>
2026-03-16 16:36:54 -0300

    feat(Application): ViewModel para alteração de senha

---
+++ CB.BackDefault.Application/Aggregates/AuthAggregate/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CB.BackDefault.Application.Aggregates.AuthAggregate.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
