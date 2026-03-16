02f46609cfab1f435352af25cc2666c52f0fe99c
carlos.bruno <carlosifto@gmail.com>
2026-03-16 18:08:28 -0300

    Chore(Identity): Separação da dependencia Identity para não poluir a Domain

---
+++ CB.BackDefault.Identity/Models/UserApplication.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CB.BackDefault.Identity.Models
+{
+    public class UserApplication : IdentityUser
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string UrlProfile { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
