205a8991f8ddbee853d61f26aaf78248ba3f0814
carlos.bruno <carlosifto@gmail.com>
2026-03-16 16:35:08 -0300

    feat(Application): response informações do usuário

---
+++ CB.BackDefault.Application/Aggregates/AuthAggregate/ViewModels/Response/UserProfileResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace CB.BackDefault.Application.Aggregates.AuthAggregate.ViewModels.Response
+{
+    public class UserProfileResponse
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string UrlProfile { get; set; }
+    }
+}
